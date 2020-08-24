dfdd75f47124ed7400d8acbd9325d87bcc896e73
Ruma Das <rumaadl16@gmail.com>
2020-08-24 23:50:47 +0930

    adding signup validations

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,6 +2,11 @@ const express = require("express");
 const { signup } = require("../controllers/auth");
 const router = express.Router();
 
-router.get("/signup", signup);
+//import validators
+
+const { userSignupValidator } = require("../validators/auth");
+const { runValidation } = require("../validators");
+
+router.post("/signup", userSignupValidator, runValidation, signup);
 
 module.exports = router;
